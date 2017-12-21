8e5e2efddfa3591179b30a64c86efbdb4df3ffca
Franco Victorio <victorio.franco@gmail.com>
2017-12-21 18:12:20 -0300

    Initial commit

---
+++ zsh-nvm.plugin.zsh
@@ -0,0 +1,29 @@
+export NVM_DIR="$HOME/.nvm"
+IS_NVM_LOADED=false
+
+loadnvm() {
+  if [ $IS_NVM_LOADED = 'false' ]
+  then
+    . "$NVM_DIR/nvm.sh"
+    IS_NVM_LOADED=true
+  fi
+}
+
+loadnvm_hook() {
+  local splitted=(${(@s/ /)1})
+  local first=$splitted[1]
+  if ! type "$first" > /dev/null; then
+    loadnvm
+  fi
+}
+
+load-nvmrc() {
+  if [ -f .nvmrc ]; then
+    loadnvm
+    nvm use
+  fi
+}
+
+add-zsh-hook chpwd load-nvmrc
+add-zsh-hook preexec loadnvm_hook
+load-nvmrc
